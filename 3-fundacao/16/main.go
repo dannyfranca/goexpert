fed9857513694453c35a2ced8145eecb796cd4bc
Danny Franca <me@dannyfranca.com>
2024-02-28 07:51:59 +0000

    docs: add comment about type assetions

--- 3-fundacao/16/main.go
+++ 3-fundacao/16/main.go
@@ -7,8 +7,8 @@ import (
 func main() {
 	var minhaVar interface{} = "Wesley Willians"
 	println(minhaVar.(string))
-	res, ok := minhaVar.(int)
-	fmt.Printf("O valor de res é %v e o resultado de ok é %v", res, ok)
-	res2 := minhaVar.(int)
-	fmt.Printf("O valor de res2 é %v", res2)
+	res, ok := minhaVar.(int) // res will be 0 and ok will be false. Good for type assertions.
+	fmt.Printf("O valor de res é %v e o resultado de ok é %v\n", res, ok)
+	res2 := minhaVar.(int) // it will panic, since we are trying to convert a string to an int and not getting the ok value.
+	fmt.Printf("O valor de res2 é %v\n", res2)
 }
