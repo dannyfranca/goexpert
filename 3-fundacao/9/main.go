2ba497002c7ed1ebfa2342e550202396ded1f6c3
Danny Franca <me@dannyfranca.com>
2024-02-28 07:16:55 +0000

    docs: add comments about variadic functions

--- 3-fundacao/9/main.go
+++ 3-fundacao/9/main.go
@@ -8,6 +8,11 @@ func main() {
 	fmt.Println(sum(1, 3, 45, 6, 34, 654, 654, 7645, 534, 543, 543, 543))
 }
 
+/**
+	This is a variadic function. It accepts any number of arguments.
+ * @param numeros
+ * @return int
+ */
 func sum(numeros ...int) int {
 	total := 0
 	for _, numero := range numeros {
