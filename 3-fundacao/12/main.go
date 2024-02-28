51ec0fc0a5d100281ad2d9a6cfc34d31aec1adec
Danny Franca <me@dannyfranca.com>
2024-02-28 07:35:22 +0000

    docs: add comment about pointers

--- 3-fundacao/12/main.go
+++ 3-fundacao/12/main.go
@@ -3,8 +3,8 @@ package main
 func main() {
 	// Memória -> Endreço -> Valor
 	a := 10
-	var ponteiro *int = &a
-	*ponteiro = 20
+	var ponteiro *int = &a // & access the address of a variable
+	*ponteiro = 20 // * access the value of the address
 	b := &a
 	*b = 30
 	println(a)
